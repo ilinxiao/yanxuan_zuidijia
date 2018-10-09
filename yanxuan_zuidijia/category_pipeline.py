fec314e95cc3b43ac6699b4250a09310cf4b0a82
linxiao <chunjinge@gmail.com>
2018-10-10 00:04:24 +0800

    TODO

--- yanxuan_zuidijia/category_pipeline.py
+++ yanxuan_zuidijia/category_pipeline.py
@@ -2,4 +2,5 @@
 class CategoryPipeline(object):
         
     def process_item(self, item, spider):
+        """ 根据item的类别 动态启用spider.settings设置ITEM_PIPEPINES """
         print("spider methods and attributes:\n%s" % dir(spider))
